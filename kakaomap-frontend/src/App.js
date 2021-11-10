5603d2532e21f49f5b1666c718b5c2a45ca0fdac
Yoon Hee Ha <yoonheeha@Yoons-MacBook-Air.local>
2021-11-10 09:42:15 +0900

    Add roadview object

--- kakaomap-frontend/src/App.js
+++ kakaomap-frontend/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Map from './Map';
+import Roadview from './Roadview';
 
 function App() {
   return (
     <>
     <div className="App">
       <Map/>
+      <Roadview/>
     </div>
 
 
